9105eb193362fba201f07eb4c1ffce4c45dcc923
Alistair English <alistair.e@greenforgelabs.com.au>
2024-05-28 17:16:21 +1000

    add arguments and launch file includes

--- clingwrap/launch_builder.py
+++ clingwrap/launch_builder.py
@@ -2,12 +2,15 @@ from contextlib import contextmanager
 
 from launch import LaunchDescription, SomeSubstitutionsType
 from launch import actions as act
+from launch import launch_description_sources
+from launch import substitutions as sub
 from launch_ros import actions as ros_act
 from launch_ros.parameters_type import SomeParameterFile, SomeParameterName, SomeParameterValue
 
 from .action_list import ActionList, ActionListImpl
+from .launch_helpers import find_file
 
-from typing import Generator, Optional
+from typing import Generator, Iterable, Optional, Text
 
 
 class LaunchBuilder(LaunchDescription):
@@ -16,6 +19,33 @@ class LaunchBuilder(LaunchDescription):
     def __init__(self):
         self._action_list = self
         super().__init__()
+
+    def declare_arg(self, name: Text, **arg_kwargs) -> sub.LaunchConfiguration:
+        self._action_list.add_action(act.DeclareLaunchArgument(name, **arg_kwargs))
+        return sub.LaunchConfiguration(name)
+
+    def declare_bool_arg(self, name: Text, **arg_kwargs) -> sub.LaunchConfiguration:
+        arg_kwargs["choices"] = ["true", "false", "True", "False"]
+        return self.declare_arg(name, **arg_kwargs)
+
+    def include_launch_py(
+        self,
+        package: str,
+        launch_file: str,
+        directory: str = "launch",
+        launch_arguments: Optional[dict[SomeSubstitutionsType, SomeSubstitutionsType]] = None,
+        **launch_kwargs,
+    ):
+        launch_file_path = find_file(package, directory, launch_file)
+        launch_arguments_tuple = [(k, v) for k, v in launch_arguments.items()] if launch_arguments is not None else None
+
+        self._action_list.add_action(
+            act.IncludeLaunchDescription(
+                launch_description_sources.PythonLaunchDescriptionSource(launch_file_path),
+                launch_arguments=launch_arguments_tuple,
+                **launch_kwargs,
+            )
+        )
 
     @contextmanager
     def namespace(self, namespace: str) -> Generator[None, None, None]:
@@ -37,7 +67,7 @@ class LaunchBuilder(LaunchDescription):
         parameters: Optional[dict[SomeParameterName, SomeParameterValue]] = None,
         parameters_file: Optional[SomeParameterFile] = None,
         remappings: Optional[list[tuple[SomeSubstitutionsType, SomeSubstitutionsType]]] = None,
-        **node_kwargs
+        **node_kwargs,
     ):
         if executable is None:
             executable = package
