467fe24f40f662b00cf2d5e391b8b3a06363be6a
Alistair English <alistair.e@greenforgelabs.com.au>
2024-05-29 16:18:30 +1000

    add composable nodes and containers

--- clingwrap/launch_builder.py
+++ clingwrap/launch_builder.py
@@ -5,16 +5,41 @@ from launch import actions as act
 from launch import launch_description_sources
 from launch import substitutions as sub
 from launch_ros import actions as ros_act
-from launch_ros.parameters_type import SomeParameterFile, SomeParameterName, SomeParameterValue
+from launch_ros import descriptions as desc
+from launch_ros.parameters_type import SomeParameterFile, SomeParameterName, SomeParameters, SomeParameterValue
+from launch_ros.remap_rule_type import SomeRemapRules
 
 from .action_list import ActionList, ActionListImpl
-from .launch_helpers import find_file
+from .launch_helpers import ContainerType, find_file
 
-from typing import Generator, Iterable, Optional, Text
+from typing import Generator, Optional, Text
+
+ComposableNodeList = list[desc.ComposableNode]
+
+
+def generate_parameter_list(
+    parameters: Optional[dict[SomeParameterName, SomeParameterValue]] = None,
+    parameters_file: Optional[SomeParameterFile] = None,
+) -> SomeParameters:
+    parameters_list = []
+    if parameters_file is not None:
+        parameters_list.append(parameters_file)
+    if parameters is not None:
+        parameters_list.append(parameters)
+    return parameters_list
+
+
+def generate_remappings_list(
+    remappings: Optional[dict[SomeSubstitutionsType, SomeSubstitutionsType]] = None
+) -> SomeRemapRules:
+    if remappings is not None:
+        return list(remappings.items())
+    return []
 
 
 class LaunchBuilder(LaunchDescription):
     _action_list: ActionList
+    _composable_node_list: Optional[ComposableNodeList] = None
 
     def __init__(self):
         self._action_list = self
@@ -66,17 +91,11 @@ class LaunchBuilder(LaunchDescription):
         executable: Optional[str] = None,
         parameters: Optional[dict[SomeParameterName, SomeParameterValue]] = None,
         parameters_file: Optional[SomeParameterFile] = None,
-        remappings: Optional[list[tuple[SomeSubstitutionsType, SomeSubstitutionsType]]] = None,
+        remappings: Optional[dict[SomeSubstitutionsType, SomeSubstitutionsType]] = None,
         **node_kwargs,
     ):
         if executable is None:
             executable = package
-
-        parameters_list = []
-        if parameters_file is not None:
-            parameters_list.append(parameters_file)
-        if parameters is not None:
-            parameters_list.append(parameters)
 
         if "emulate_tty" not in node_kwargs and "output" not in node_kwargs:
             # https://github.com/ros2/launch/issues/188
@@ -87,8 +106,65 @@ class LaunchBuilder(LaunchDescription):
             ros_act.Node(
                 package=package,
                 executable=executable,
-                parameters=parameters_list,
-                remappings=remappings,
+                parameters=generate_parameter_list(parameters, parameters_file),
+                remappings=generate_remappings_list(remappings),
+                **node_kwargs,
+            )
+        )
+
+    @contextmanager
+    def composable_node_container(
+        self,
+        name: str,
+        container_type: ContainerType = ContainerType.SINGLE_THREAD_EACH,
+        parameters: Optional[dict[SomeParameterName, SomeParameterValue]] = None,
+        parameters_file: Optional[SomeParameterFile] = None,
+        remappings: Optional[dict[SomeSubstitutionsType, SomeSubstitutionsType]] = None,
+        **container_kwargs,
+    ) -> Generator[None, None, None]:
+        if self._composable_node_list is not None:
+            raise ValueError("A composable node container is already in context!")
+
+        self._composable_node_list = []
+
+        yield
+
+        executable, args = container_type.value
+        container_kwargs["arguments"] = container_kwargs.get("arguments", []) + args
+
+        self._action_list.add_action(
+            ros_act.ComposableNodeContainer(
+                package="rclcpp_components",
+                executable=executable,
+                name=name,
+                namespace="",
+                composable_node_descriptions=self._composable_node_list,
+                parameters=generate_parameter_list(parameters, parameters_file),
+                remappings=generate_remappings_list(remappings),
+                **container_kwargs,
+            )
+        )
+
+        self._composable_node_list = None
+
+    def composable_node(
+        self,
+        package: str,
+        plugin: str,
+        parameters: Optional[dict[SomeParameterName, SomeParameterValue]] = None,
+        parameters_file: Optional[SomeParameterFile] = None,
+        remappings: Optional[dict[SomeSubstitutionsType, SomeSubstitutionsType]] = None,
+        **node_kwargs,
+    ):
+        if self._composable_node_list is None:
+            raise ValueError("There is no composable node container in context!")
+
+        self._composable_node_list.append(
+            desc.ComposableNode(
+                package=package,
+                plugin=plugin,
+                parameters=generate_parameter_list(parameters, parameters_file),
+                remappings=generate_remappings_list(remappings),
                 **node_kwargs,
             )
         )
--- clingwrap/launch_helpers.py
+++ clingwrap/launch_helpers.py
@@ -1,8 +1,25 @@
+from enum import Enum
+
 from launch import SomeSubstitutionsType
 from launch import substitutions as sub
 from launch.utilities.type_utils import SomeValueType
 from launch_ros import parameter_descriptions as param
 from launch_ros import substitutions as ros_sub
+
+
+class ContainerType(Enum):
+    """
+    Composable node container type.
+    """
+
+    # All components using one single-threaded executor
+    SINGLE_THREAD = ("component_container", [])
+    # Each component gets its own single-threaded executor
+    SINGLE_THREAD_EACH = ("component_container_isolated", [])
+    # Each component gets its own multi-threaded executor
+    MULTI_THREAD_EACH = ("component_container_isolated", ["--use_multi_threaded_executor"])
+    # All components using one multi-threaded executor
+    MULTI_THREAD = ("component_container_mt", [])
 
 
 def find_file(
