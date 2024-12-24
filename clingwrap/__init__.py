b6a544cdc8fccd1eae1543205fc7fb4c7cd7b52b
Alistair English <alistair.e@greenforgelabs.com.au>
2024-12-24 11:41:56 +1000

    add diagonal covariance parameter helper

--- clingwrap/__init__.py
+++ clingwrap/__init__.py
@@ -1,2 +1,3 @@
 from .launch_builder import LaunchBuilder
 from .launch_helpers import *
+from .param_helpers import *
---
+++ clingwrap/param_helpers.py
@@ -0,0 +1,2 @@
+def cov_xyz_diag(x: float, y: float, z: float) -> list[float]:
+    return [x, 0, 0, 0, y, 0, 0, 0, z]
